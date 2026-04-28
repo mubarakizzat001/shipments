d3f567be2c7487721b26dd10041271d8c76a0e17
mubaraizzat <mubaraizzat@gmail.com>
2026-04-28 09:20:42 +0400

    feat: add deliverypartner model and update shipment schema with migration support

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from app.database.models import Shipment,Seller
+from app.database.models import *
 from app.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
